fa991a4b9e16af35a1d7f1d9afae0f0d6acffcde
Katawoot Keawnil <Kxtawoot2003@Gmail.com>
2025-05-04 19:37:49 +0700

    Enemy เดินได้

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Vector2 velocity;
+    [SerializeField] private Transform[] movePoints;
+    Rigidbody2D rb;
     public int health = 10;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Behaviour();
+    }
     public void TakeDamage(int amount)
     {
         health -= amount;
@@ -14,5 +25,28 @@ public class Enemy : MonoBehaviour
             Debug.Log("Enemy defeated!");
             Destroy(gameObject); // ทำลายศัตรูเมื่อ HP หมด
         }
+    }
+
+    public void Behaviour()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+        if (rb.position.x <= movePoints[0].position.x && velocity.x < 0)
+        {
+            FlipCharacter();
+        }
+
+        else if (rb.position.x >= movePoints[1].position.x && velocity.x > 0)
+        {
+            FlipCharacter();
+        }
+    }
+    private void FlipCharacter()
+    {
+        velocity *= -1;
+
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
